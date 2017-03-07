b19633c32ddb1a2b6cb35c1a98be749ef0f2e083
sudozgj <13714515160@163.com>
2017-03-07 15:51:52 +0800

    数据库表修改，同步

--- geti.sql
+++ geti.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50535
 File Encoding         : 65001
 
-Date: 2017-03-06 18:38:21
+Date: 2017-03-07 15:51:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,23 +22,19 @@ CREATE TABLE `interface` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `input` varchar(255) DEFAULT '',
-  `output` varchar(255) NOT NULL,
+  `output` varchar(1000) NOT NULL,
   `description` varchar(255) DEFAULT '',
   `time` bigint(20) NOT NULL,
   `m_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of interface
 -- ----------------------------
-INSERT INTO `interface` VALUES ('7', 'login', 'wef', 'sdfsf', 'ewf', '1488786941', '15');
-INSERT INTO `interface` VALUES ('8', 'register', '1', '1', '1', '1488786985', '15');
-INSERT INTO `interface` VALUES ('9', 'List', '1', '1', '1', '1488786994', '17');
-INSERT INTO `interface` VALUES ('10', 'getLogList', '1', '1', '1', '1488787002', '18');
-INSERT INTO `interface` VALUES ('11', 'mxr', 'd', 'd', 'd', '1488790618', '15');
-INSERT INTO `interface` VALUES ('12', 'haha', '2', '3', '4', '1488792094', '19');
-INSERT INTO `interface` VALUES ('13', 'getInterface', 'iName', '{\n   \"data\": {     \"id\": 10,     \"name\": \"getLogList\",     \"input\": \"1\",     \"output\": \"1\",     \"description\": \"1\",     \"time\": 1488787002,     \"mid\": 10008   }, \n  \"code\": 1,   \"msg\": \"获取接口信息成功\" }', '获取接口对象', '1488795000', '21');
+INSERT INTO `interface` VALUES ('18', 'getServerInfo', ' ', '[{\"cpu_num\": \"2\",\"mem_total\": \"3.7543\",\"cpu_percent\": \"21.1703\",\"user_login\": \"0\",\"net_i\": \"1073.8203\",\"disk_total\": \"454.4951\",\"cpu_load_5min\": \"0.29\",\"disk_free\": \"285.7523\",\"mem_used\": \"2.0375\",\"runtime\": \"2526591\",\"net\": \"2061.2609\",\"cpu_load_15min\": \"0.34\",\"mem_pused\": \"49.0445\",\"net_o\": \"1018.4984\",\"mem_free\": \"1.8427\",\"ip\": \"192.168.1.117\",\"disk_pused\": \"33.7595\",\"mac_address\": \"[eth0] b8:ae:ed:bd:2b:0a\",\"disk_used\": \"145.6339\",\"cpu_load_1min\": \"0.26\",\"proc_num\": \"178\"}]', '获取服务器全部信息', '1488869951', '28');
+INSERT INTO `interface` VALUES ('19', 'login', '{username:xxx,password:xxx}', '[\n    {\n        \"cpu_num\": \"2\",\n        \"mem_total\": \"3.7543\",\n        \"cpu_percent\": \"21.1703\",\n        \"user_login\": \"0\",\n        \"net_i\": \"1073.8203\",\n        \"disk_total\": \"454.4951\",\n        \"cpu_load_5min\": \"0.29\",\n        \"disk_free\": \"285.7523\",\n        \"mem_used\": \"2.0375\",\n        \"runtime\": \"2526591\",\n        \"net\": \"2061.2609\",\n        \"cpu_load_15min\": \"0.34\",\n        \"mem_pused\": \"49.0445\",\n        \"net_o\": \"1018.4984\",\n        \"mem_free\": \"1.8427\",\n        \"ip\": \"192.168.1.117\",\n        \"disk_pused\": \"33.7595\",\n        \"mac_address\": \"[eth0] b8:ae:ed:bd:2b:0a\",\n        \"disk_used\": \"145.6339\",\n        \"cpu_load_1min\": \"0.26\",\n        \"proc_num\": \"178\"\n    }\n]', '阿斯达', '1488872474', '29');
+INSERT INTO `interface` VALUES ('20', 'getURList', 'username,password', '[\n{\n\"id\": \"1\",\n\"username\": \"admin\",\n\"name\": \"张三\",\n\"role\": \"超级管理员\"\n},\n{\n\"id\": \"2\",\n\"username\": \"zgj\",\n\"name\": \"李四\",\n\"role\": \"角色0002\"\n},\n{\n\"id\": \"3\",\n\"username\": \"mxr\",\n\"name\": \"mxr\",\n\"role\": \"\"\n},\n{\n\"id\": \"140\",\n\"username\": \"z\",\n\"name\": \"小明\",\n\"role\": \"小兵\"\n},\n{\n\"id\": \"141\",\n\"username\": \"g\",\n\"name\": \"王五\",\n\"role\": \"营长\"\n}\n]', '获取用户角色关联信息列表', '1488872437', '30');
 
 -- ----------------------------
 -- Table structure for `module`
@@ -49,16 +45,15 @@ CREATE TABLE `module` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of module
 -- ----------------------------
-INSERT INTO `module` VALUES ('15', 'User', '');
-INSERT INTO `module` VALUES ('17', 'List', 'ef');
-INSERT INTO `module` VALUES ('18', 'Log', 'ef');
-INSERT INTO `module` VALUES ('19', '哈哈', 'asdsad');
-INSERT INTO `module` VALUES ('20', 'jack', 'ef');
+INSERT INTO `module` VALUES ('28', '网络主机模块', '网络主机的相关CPU，内存，磁盘，网络参数信息');
+INSERT INTO `module` VALUES ('29', '用户管理模块', '用户');
+INSERT INTO `module` VALUES ('30', '权限管理模块', '权限管理模块，...');
+INSERT INTO `module` VALUES ('31', '小龙模块', '111');
 
 -- ----------------------------
 -- Table structure for `test`
